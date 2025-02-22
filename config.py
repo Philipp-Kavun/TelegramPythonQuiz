5d142acb3e66cda06aa536fefbca843bdf282685
Philipp-Kavun <oaspido@gmail.com>
2025-02-22 16:50:28 +0300

    emove config.py from repository and add to .gitignore

--- config.py
+++
@@ -1,8 +0,0 @@
-# Файл, содержащий токен бота и другие настройки
-from aiogram import Bot
-
-# Замените "YOUR_BOT_TOKEN" на токен, который вы получили от BotFather
-API_TOKEN = '8087358950:AAGOQzYK1hlr5i8PNCB0W8LfTUapZkZaju8'
-
-# Объект бота
-bot = Bot(token=API_TOKEN)
